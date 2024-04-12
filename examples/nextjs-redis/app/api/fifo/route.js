12032c1dc8e6d5f957c1641c5304579c6794740a
michaelwitk <mwit2023@gmail.com>
2024-04-12 11:44:10 +0200

    log

--- examples/nextjs-redis/app/api/fifo/route.js
+++ examples/nextjs-redis/app/api/fifo/route.js
@@ -2,5 +2,10 @@ import { authorization_verify } from '../../../libs/auth'
 
 export const POST = async (req) => {
   authorization_verify()
-  return Response.json({})
+
+  let startedAt = Date.now()
+  await new Promise((r) => setTimeout(r, 1000 * 1))
+  let finishedAt = Date.now()
+
+  return Response.json({ startedAt, finishedAt })
 }
